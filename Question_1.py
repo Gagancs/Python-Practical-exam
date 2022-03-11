0e3a052c91fb270fa659168c37dff6c3fbd821bc
Gagancs <96284939+Gagancs@users.noreply.github.com>
2022-03-11 12:47:32 +0530

    Update Question_1.py

--- Question_1.py
+++ Question_1.py
@@ -2,9 +2,9 @@
 
 Write a function that finds the sum of the
 
-a) first n odd terms
+a) first n even terms
 
-b) first n even terms
+b) first n odd terms
 
 c) 1, 2, 4, 3, 5, 7, 9, 6, 8, 10, 11, 13.. till n-th term
 """
